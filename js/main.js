5da94223d778dd7806ddb862179cb362ca6c0205
F1rman <ihorkharchyshyn@gmail.com>
2020-08-21 16:39:44 +0300

    new slider_s4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 $(document).ready(()=>{
- var swiper = new Swiper('.swiper-container', {
+ var swiper = new Swiper('#main_slider', {
     mousewheel: true,
     spaceBetween:800,
-   hashNavigation: true,
+    hashNavigation: {
+     replaceState: true,
+     watchState:true,
+   },
+    freeMode:true
+
+ });
+ var swiper_s4 = new Swiper('#s4 .swiper-container', {
+    mousewheel: true,
+     // direction: 'vertical',
+     nested:true,
+     autoHeight: true,
+     slidesPerView:3,
+     centeredSlides:true,
+      parallax: true,
     // freeMode:true
  });
+ $('.next_page_btn').click(()=>{
+   swiper.slideNext();
+
+ })
+ $('.btn_down').click(()=>{
+   swiper_s4.slideNext();
+
+ })
+
+ $('#menu  a').click(function(){
+   $('#menu  a').removeClass("active");
+   $(this).addClass("active");
+});
 })
