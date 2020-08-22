dd7264f5a626b5d164169db12ba52f3de04f18cb
F1rman <ihorkharchyshyn@gmail.com>
2020-08-22 19:29:54 +0300

    decktop

--- js/main.js
+++ js/main.js
@@ -6,23 +6,33 @@ $(document).ready(()=>{
      replaceState: true,
      watchState:true,
    },
-    freeMode:true
-
+   direction: 'horizontal',
+    freeMode:true,
  });
+
+
  var swiper_s4 = new Swiper('#s4 .swiper-container', {
     mousewheel: true,
-     // direction: 'vertical',
      nested:true,
      autoHeight: true,
      slidesPerView:3,
      centeredSlides:true,
       parallax: true,
+      direction: 'horizontal',
+      mousewheel: true,
+       roundLengths: true,
+       grabCursor: true,
     // freeMode:true
  });
+
+
  $('.next_page_btn').click(()=>{
    swiper.slideNext();
-
  })
+ $('#back_page_btn').click(()=>{
+   swiper.slidePrev();
+ })
+
  $('.btn_down').click(()=>{
    swiper_s4.slideNext();
 
